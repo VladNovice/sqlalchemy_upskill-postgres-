42f9652150b11b02258740d6c355800578029df4
Vlad Novice <hakettip@mail.ru>
2025-03-08 21:42:02 +0500

    GOOD NIGHT

--- src/database.py
+++ src/database.py
@@ -1,16 +1,22 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
-from sqlalchemy.orm import Session, sessionmaker
-from sqlalchemy import URL, create_engine, text
-from src.config import settings
+# database.py
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy import text
+from config import settings
+import asyncio
 
+# Создаем асинхронный движок
 engine = create_async_engine(
     url=settings.DATABASE_URL_asyncpg, 
-    echo=True,  # Консоль будет сыпать все запросы которые делает sql
+    echo=True,
     pool_size=5,
     max_overflow=10,
 )
 
+async def test_connection():
+    async with engine.begin() as conn:
+        result = await conn.execute(text("SELECT version()"))
+        print("PostgreSQL version:", result.scalar())
 
-with engine.begin() as conn:
-    res = conn.execute(text("SELECT VERSION()"))
-    print(f"{res=}")
+if __name__ == "__main__":
+    # Запускаем асинхронную функцию через event loop
+    asyncio.run(test_connection())
