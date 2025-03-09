4ec188ce1fb6d6b1bb8997bc85c84f50e97633e0
Vlad Novice <hakettip@mail.ru>
2025-03-09 18:53:54 +0500

    dfg

--- src/config.py
+++ src/config.py
@@ -12,7 +12,10 @@ class Settings(BaseSettings):
     def DATABASE_URL_asyncpg(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
     
-
+    
+    @property
+    def DATABASE_URL_psycopg(self):
+        return f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
 
     model_config = SettingsConfigDict(env_file=".env")
