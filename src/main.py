9f5f4fb5c435d06f9f43aef19c4663178f8b3eb6
Vlad Novice <hakettip@mail.ru>
2025-03-15 17:09:57 +0500

    less - true

--- src/main.py
+++ src/main.py
@@ -2,9 +2,10 @@ import os
 import sys
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
+import asyncio
 
 from queries.orm import create_tables, insert_data
 
 
 create_tables()
-insert_data()
+asyncio.run(insert_data())
--- src/queries/orm.py
+++ src/queries/orm.py
@@ -1,5 +1,5 @@
 from sqlalchemy import text, insert
-from database import sync_engine, async_engine, session_factory
+from database import sync_engine, async_engine, session_factory, async_session_factory
 from models import metadata_obj, WorkersOrm
 
 
@@ -18,3 +18,10 @@ def insert_data():
         worker_vlad = WorkersOrm(username="Vlad")
         session.add_all([worker_vlad, worker_arthur])
         session.commit()
+
+async def insert_data():
+    async with session_factory() as session: 
+        worker_arthur = WorkersOrm(username="Arthur")
+        worker_vlad = WorkersOrm(username="Vlad")
+        session.add_all([worker_vlad, worker_arthur])
+        await session.commit()
