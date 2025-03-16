ef3af5368ef90ba6984450ee8d46fcf90230a999
Vlad Novice <hakettip@mail.ru>
2025-03-16 12:36:10 +0500

    datatime intpk

--- src/models.py
+++ src/models.py
@@ -1,15 +1,53 @@
-from sqlalchemy import Table, Column, Integer, String, MetaData
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, func, text
 from sqlalchemy.orm import Mapped, mapped_column
+
+from typing import Annotated
+import enum
+import datetime
+
 from database import Base
+
+
+
+
+intpk = Annotated[int, mapped_column(primary_key=True)]
+
+created_at = Annotated[datetime.datetime, mapped_column(server_default=text("TIMEZONE('utc', now())"))] 
+updated_at = Annotated[datetime.datetime, mapped_column(
+        server_default=text("TIMEZONE('utc', now())"),
+        onupdate=datetime.datetime.utcnow, 
+    )]
+
+
 
 
 class WorkersOrm(Base):
     __tablename__ = "workers"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[intpk]
     username: Mapped[str] = mapped_column()
 
+class WorkLoad(enum.Enum):
+    parttime = "parttime"
+    fulltime = "fulltime"
 
+
+class ResumesOrm(Base):
+    __tablename__ = "resumes"
+
+    id: Mapped[intpk]
+    title: Mapped[str] = mapped_column()
+    compensation: Mapped[int | None]
+    workload: Mapped[WorkLoad]
+    worker_id: Mapped[int] = mapped_column(ForeignKey("workers.id"), ondelete='CASCADE') 
+    created_at: Mapped[datetime.datetime] = mapped_column(server_default=text("TIMEZONE('utc', now())"))
+    updated_at: Mapped[datetime.datetime] = mapped_column(
+        server_default=text("TIMEZONE('utc', now())"),
+        onupdate=datetime.datetime.utcnow, 
+    )
+    
+
+    
 
 
 
