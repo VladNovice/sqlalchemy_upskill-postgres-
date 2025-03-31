a5504944a1885bffa6e699ef28062697cfe59557
Vlad Novice <hakettip@mail.ru>
2025-03-31 21:24:52 +0500

    async

--- src/queries/core.py
+++ src/queries/core.py
@@ -16,23 +16,40 @@ async def get_123_async():
 
 
 
-def create_tables():
-    sync_engine.echo = False
-    metadata_obj.drop_all(sync_engine)
-    metadata_obj.create_all(sync_engine)
-    sync_engine.echo = True
+
+class SyncCore:
+    @staticmethod
+    def create_tables():
+        sync_engine.echo = False
+        metadata_obj.drop_all(sync_engine)
+        metadata_obj.create_all(sync_engine)
+        sync_engine.echo = True
+
+    @staticmethod
+    def insert_data():
+        with sync_engine.connect() as conn:
+            #stmt = """INSERT INTO workers (username) VALUES
+            #    ('AO Bobr'),
+            #    ('OOO Volk');"""
+            stmt = insert(worker_table).values(
+                [
+                    {"username": "Bobr"},
+                    {"username": "Volk"},
+                ]
+            )
+            conn.execute(stmt)
+            conn.commit()
 
 
-def insert_data():
-    with sync_engine.connect() as conn:
-        #stmt = """INSERT INTO workers (username) VALUES
-        #    ('AO Bobr'),
-        #    ('OOO Volk');"""
-        stmt = insert(worker_table).values(
-            [
-                {"username": "Bobr"},
-                {"username": "Volk"},
-            ]
-        )
-        conn.execute(stmt)
-        conn.commit()
+
+
+class AsyncCore:
+    @staticmethod
+    # Асинхронный вариант
+    async def create_tables():
+        async with async_engine.begin() as conn:
+            await conn.run_sync(metadata_obj.drop_all)
+            await conn.run_sync(metadata_obj.create_all)
+
+
+
